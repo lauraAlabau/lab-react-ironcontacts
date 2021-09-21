2f3e14f7e434550b250b0908312fe74ab18bd261
lauraAlabau <arlaura3@gmail.com>
2021-09-21 15:32:32 +0200

    Iteration 2 | Conditionally Display Awards Info

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import contacts from "./contacts.json";
 
 function App() {
-  //console.log(contacts)
+  console.log(contacts)
   //console.log(contacts[0])
   //console.log(contacts[0].name)
   //console.log(contacts.splice(0,5))
@@ -14,6 +14,8 @@ function App() {
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
         </tr>
         {contacts.splice(0,5).map((element) => {
           return(
@@ -21,6 +23,8 @@ function App() {
               <td><img src={element.pictureUrl} alt=""/></td>
               <td>{element.name}</td>
               <td>{element.popularity}</td>
+              <td>{element.wonOscar ? "🏆" : ""}</td>
+              <td>{element.wonEmmy  ? "" : "🏆"}</td>
             </tr>
           )
         })}
