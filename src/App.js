c16925b6e4aa6f93d3d013be541cafe23c678ccc
lauraAlabau <arlaura3@gmail.com>
2021-09-21 18:13:08 +0200

    Iteration 5 | Remove Contacts and FIXED Iteration 3 | Add New Random Contacts

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import contacts from "./contacts.json";
 import {useState} from 'react';
 
 
-
+const listContacts = [...contacts]
+const spliceList = listContacts.splice(0,5)
 
 function App() {
-  console.log(contacts)
+  //console.log(contacts)
   //console.log(contacts[0])
   //console.log(contacts[0].name)
   //console.log(contacts.splice(0,5))
-  const [contact, setContact] = useState(contacts.splice(0,5))
+
+  const [contact, setContact] = useState(spliceList)
 
   function addRandom(){
-    const randomContact = contacts[Math.floor(Math.random()*contacts.length)] // Me devuelve un objeto random
-    //console.log(randomContact.name) 
-    const newArray = [...contact] // Duplico el array original de contact
-    newArray.push(randomContact)  //Añado el elemento random a ese array nuevo
-    setContact(newArray) 
+    const randomContact = listContacts.splice(Math.floor(Math.random()*listContacts.length),1) // Me devuelve un objeto random
+    console.log(randomContact) 
+    console.log(listContacts)
+    //const newArray = [...contact] // Duplico el array original de contact
+    //newArray.push(randomContact[0])  //Añado el elemento random a ese array nuevo
+    setContact([...contact, randomContact[0]]) 
   }
 
   function sortByPopularity(){
@@ -41,25 +44,24 @@ function App() {
     });
     setContact(sortByName) 
   }
- 
+
+  function deleteContact(id){
+    const newArray = [...contact] 
+    const filteredContacts = newArray.filter(element => {
+        return id !== element.id;
+      });
+      setContact(filteredContacts) 
+  }
+
 
   return (
     <div className="App">
+
       <h1>Iron Contacts</h1>
-       <button onClick={() =>{
-          addRandom()
-      }}>Add Random Contact
-      </button>
 
-      <button onClick={() =>{
-          sortByPopularity()
-      }}>Sort by popularity
-      </button>
-
-      <button onClick={() =>{
-          sortByName()
-      }}>Sort by name
-      </button>
+      <button onClick={() =>{addRandom()}}>Add Random Contact</button>
+      <button onClick={() =>{sortByPopularity()}}>Sort by popularity</button>
+      <button onClick={() =>{sortByName()}}>Sort by name</button>
 
       <table>
         <tr>
@@ -68,15 +70,19 @@ function App() {
           <th>Popularity</th>
           <th>Won an Oscar</th>
           <th>Won an Emmy</th>
+          <th>Actions</th>
         </tr>
         {contact.map((element) => {
           return(
-            <tr key= {element.id}>
-              <td><img src={element.pictureUrl} alt=""/></td>
-              <td>{element.name}</td>
-              <td>{element.popularity}</td>
-              <td>{element.wonOscar ? "🏆" : ""}</td>
-              <td>{element.wonEmmy  ? "🏆" : ""}</td>
+            <tr key= {element?.id}>
+              <td><img src={element?.pictureUrl} alt=""/></td>
+              <td>{element?.name}</td>
+              <td>{element?.popularity}</td>
+              <td>{element?.wonOscar ? "🏆" : ""}</td>
+              <td>{element?.wonEmmy  ? "🏆" : ""}</td>
+              <td>  
+                <button onClick={() =>{deleteContact(element.id)}}>Delete</button> 
+              </td>
             </tr>
           )
         })}
