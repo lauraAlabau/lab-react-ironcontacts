de19edb03b2dc35867bf3a5fb5146f492e58d135
lauraAlabau <arlaura3@gmail.com>
2021-09-21 17:09:46 +0200

    Iteration 4 | Sort Contacts by Name and Popularity

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useState} from 'react';
 
 
 
+
 function App() {
   console.log(contacts)
   //console.log(contacts[0])
@@ -12,11 +13,33 @@ function App() {
   const [contact, setContact] = useState(contacts.splice(0,5))
 
   function addRandom(){
-    const randomContact = contacts[Math.floor(Math.random()*contacts.length)] // Me devuelve un objeto
+    const randomContact = contacts[Math.floor(Math.random()*contacts.length)] // Me devuelve un objeto random
     //console.log(randomContact.name) 
-    const newArray = [...contact]
-    newArray.push(randomContact)
-    setContact(newArray)
+    const newArray = [...contact] // Duplico el array original de contact
+    newArray.push(randomContact)  //Añado el elemento random a ese array nuevo
+    setContact(newArray) 
+  }
+
+  function sortByPopularity(){
+    const newArray = [...contact] 
+    const sortByPopularity = newArray.sort(function (a, b) {
+      return b.popularity - a.popularity;
+    });
+    setContact(sortByPopularity) 
+  }
+
+  function sortByName(){
+    const newArray = [...contact] 
+    const sortByName = newArray.sort(function (a, b) {
+      if(a.name < b.name){ 
+        return -1; 
+      }
+      if(a.name > b.name){ 
+        return 1; 
+      }
+      return 0;
+    });
+    setContact(sortByName) 
   }
  
 
@@ -26,6 +49,16 @@ function App() {
        <button onClick={() =>{
           addRandom()
       }}>Add Random Contact
+      </button>
+
+      <button onClick={() =>{
+          sortByPopularity()
+      }}>Sort by popularity
+      </button>
+
+      <button onClick={() =>{
+          sortByName()
+      }}>Sort by name
       </button>
 
       <table>
